b993e9f65cab72e1cebf675047614494d21f2eea
iansm <ians.mcvay@gmail.com>
2025-09-28 16:48:47 -0700

    Add function for selecting computer move.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,18 @@
 // Get the computer choice
 
 function getComputerChoice() {
-
+    let computerChoice = Math.random();
+    if (computerChoice <= 0.33) {
+        return "rock";
+    } else if (computerChoice <= 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
+
+
+console.log(getComputerChoice());
 
 // Get the human choice
 // Declare the players scores as variables
