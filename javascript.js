b708cbc9862ff19295b23c54e89d6ae1932f8f08
iansm <ians.mcvay@gmail.com>
2025-10-04 15:55:37 -0700

    Add logic for playRound. Update global variables. console.log to see score updates.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,4 @@
 // Get the computer choice
-
 function getComputerChoice() {
     let computerChoice = Math.random();
     if (computerChoice <= 0.33) {
@@ -11,22 +10,41 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
-
 // Get the human choice
-
 function getHumanChoice() {
     let humanChoice = prompt("Enter rock, paper, or scissors:").toLowerCase();
     return humanChoice;
 }
 
-console.log(getHumanChoice());
-
 // Declare the players scores as variables
-
 let humanScore = 0;
 let computerScore = 0;
+
 // Play a single round
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return "It's a tie!";
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") || 
+        (humanChoice === "scissors" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "rock")
+    ) {
+        humanScore++;
+        return `You win! ${humanChoice} beats ${computerChoice}.`
+    } else {
+        computerScore++;
+        return `You lose! ${computerChoice} beats ${humanChoice}.`
+    }
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+const result = playRound(humanSelection, computerSelection);
+
+console.log(result);
+console.log(`Your Score: ${humanScore}\nComputer Score: ${computerScore}`)
+
 // Now play 5 rounds per game
 
 
